040bfb59eca07c6f292a36a409217c74e481f79c
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-15 04:04:51 +0300

    feat(core): establish connection with mongodb

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const UserModel = require('./models/Users')
+
+
 
 const app = express
 
+app.use(express.json())
+
+
 mongoose.connect('mongodb://localhost:27017/E-Commerce')
+
+app.post("/createUser",(req,res)=>{
+    UserModel.create(req.body)
+        .then(users=>res.json(users))
+        .catch(err=>res.json(err))
+})
+
 
 app.listen(3001,()=>{
     console.log("server is running")
