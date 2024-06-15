26500538e237f59ad2fef9fb65c83cd7aee09018
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-15 18:32:24 +0300

    feat(core): add middleware to server

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,11 @@ const express = require('express')
 //express app
 const app = express()
 
+//middleware
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
 
 //Routes
 app.get('/',(req,res)=>{
