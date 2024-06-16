31ffe159607fbd66c48210b82b8276e12d5dc7dd
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-16 16:58:58 +0300

    feat(core): add delete and update controls

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,6 +1,7 @@
 const { default: mongoose } = require('mongoose')
 const userModel = require('../Models/UserModel')
 const mangoose = require('mongoose')
+const { json } = require('express')
 
 //get all users
 const getUsers = async(req,res)=>{
@@ -42,12 +43,42 @@ const createUser = async(req,res)=>{
 }
 
 //delete a user
+const deleteUser = async(req,res)=>{
+    const { id } = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:'invalid id'})
+    }
 
+    const user = await userModel.findOneAndDelete({_id:id})
+    
+    if(!user){
+        return res.status(400).json({error:'No user found'})
+    }
+    res.status(200).json(user)
+}
 
 //update user info
+const updateUser = async(req,res)=>{
+    const { id } = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:'invalid id'})
+    }
+    const user = await userModel.findOneAndUpdate({_id:id},{
+        ...req.body
+    })
+
+    if(!user){
+        res.status(400).json({error:'No such user'})
+    }
+    return res.status(200).json(user)
+}
+
+
 
 module.exports={
     createUser,
     getUsers,
-    getUser
+    getUser,
+    deleteUser,
+    updateUser
 }
