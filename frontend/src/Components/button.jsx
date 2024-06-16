da61a8fecdc2d4cc5e95f8c8b125f170f7ca50d2
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-16 23:30:32 +0300

    feat(user-interface): create button component

---
+++ frontend/src/Components/button.jsx
@@ -0,0 +1,16 @@
+
+import './components.css';
+function Button(){
+
+    return(
+        <div className="create-account-submission">
+            <button className="create-account-submission_btn">
+                <p style={{color:'white',fontWeight:700,fontSize:'20px'}}>Create Account</p>
+            </button>
+        </div>
+       
+    );
+
+}
+
+export default Button;
