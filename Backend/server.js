25319bf5b18bdc31285924b92b543a9ac6c7299e
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-16 12:00:45 +0300

    feat(core): add working database connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,36 @@
 require('dotenv').config()
 const express = require('express')
-
+const userRoutes = require('./Routes/users')
+const mongoose = require('mongoose')
 
 //express app
 const app = express()
 
 //middleware
+app.use(express.json())
+
 app.use((req,res,next)=>{
     console.log(req.path,req.method)
     next()
 })
 
 //Routes
-app.get('/',(req,res)=>{
-    res.json({mssg:"Welcome to the home page"})
-})
+app.use('/api/users/',userRoutes)
 
-//listen for requests
-app.listen(process.env.PORT,()=>{
-    console.log("server is running on port ",process.env.PORT)
-})
+
+//connect to database
+mongoose.connect(process.env.MONG_URI)
+    .then(()=>{
+        //listen for requests
+    app.listen(process.env.PORT,()=>{
+        console.log("server is running on port ",process.env.PORT)
+    })
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
+
+
 
 
