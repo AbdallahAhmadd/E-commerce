f3e8d1d9a27ab0580c533337523fd516d05772a1
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-17 08:41:19 +0300

    feat(core): add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
-import Button from './Components/Button/button.jsx';
+
+import LeftBox from './Components/MainBox/LeftBox.jsx';
+import RightBox from './Components/MainBox/RightBox.jsx';
+import SignupPage from './Pages/SignupPage.jsx';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
 function App() {
   return (
     <div className="App">
-     <Button/>
+      <BrowserRouter>
+      <div className='pages'>
+      <Routes>
+        <Route path="/"
+        element={<SignupPage />}
+        />
+      </Routes>
+      </div>
+    
+      </BrowserRouter>
+      
     </div>
   );
 }
