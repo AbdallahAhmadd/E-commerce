0aa4ce4926df61de1663bd128b892b1dee267416
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-17 04:06:56 +0300

    fix(component): fix margins and spacing

--- frontend/src/Components/TextBox/Textbox.jsx
+++ frontend/src/Components/TextBox/Textbox.jsx
@@ -4,10 +4,10 @@ function Textbox(props){
     const {isHalfWidth=false , placeholder='Enter Text'}= props;
 
     return(
-    <div className='textbox-container'>
+    <div className='textbox-container'   style={{width:isHalfWidth? '48%':'100%'}}>
         <input type='text' placeholder={placeholder}
                 className='textbox-container_input' 
-                style={{width:isHalfWidth? '50%':'100%'}}
+              
         />
     </div>
     );
