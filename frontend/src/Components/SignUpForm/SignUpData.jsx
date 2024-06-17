cebc6f104c2fa14076b8c7118ff32726d6aba62f
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-17 04:07:46 +0300

    feat(component): create signup form

---
+++ frontend/src/Components/SignUpForm/SignUpData.jsx
@@ -0,0 +1,21 @@
+import Textbox from "../TextBox/Textbox";
+import Button from "../Button/button";
+import './signupdata.css';
+
+function SignUpData(){
+    return(
+        <div className='sign-up-data'>
+            <div className="sign-up-data_name">
+                <Textbox isHalfWidth={true} placeholder='First Name' />
+                <Textbox isHalfWidth={true} placeholder='Last Name'/>
+            </div>
+            <div className="sign-up-data_credentials">
+                <Textbox placeholder='Email'/>
+                <Textbox placeholder='Password'/>
+                <Button/>
+            </div>
+        </div>
+    );
+}
+
+export default SignUpData;
