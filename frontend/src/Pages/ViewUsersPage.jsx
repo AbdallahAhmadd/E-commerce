cc1b6450e69f65ae663634c9eb5bba353f72d672
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-18 04:31:52 +0300

    feat(UI): create view user info page

---
+++ frontend/src/Pages/ViewUsersPage.jsx
@@ -0,0 +1,28 @@
+import ViewInfoCard from "../Components/ViewInfoBox/ViewInfoCard";
+import './viewuserspage.css'
+import AnalysisImage from './analysis-woman.png'
+function ViewUsersPage() {
+    return(
+    <>
+        <div className="view-users-header">
+            <p>View Users</p>
+        </div>
+        <div style={{display:'flex',flexDirection:'row'}}>
+            <div className="info-cards-container">
+                <ViewInfoCard />
+                <ViewInfoCard />
+                <ViewInfoCard />
+                <ViewInfoCard />
+                <ViewInfoCard />
+            </div>
+
+            <div className="view-info-image-container">
+                <img src={AnalysisImage} alt="woman working on a computer" />
+            </div>
+        </div>
+        
+    </>
+    );
+}
+
+export default ViewUsersPage;
