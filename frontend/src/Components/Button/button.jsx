ebba06b02c1ed5384a99614c3c3c095d3f1cbbb5
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-18 05:16:42 +0300

    fix(component): add text prop to button

--- frontend/src/Components/Button/button.jsx
+++ frontend/src/Components/Button/button.jsx
@@ -1,11 +1,11 @@
 
 import './button.css';
 function Button(props){
-    const {onClick} = props;
+    const {onClick,text='Create Account'} = props;
     return(
         <div className="create-account-submission">
             <button className="create-account-submission_btn" onClick={onClick}>
-                <p style={{color:'white',fontWeight:700,fontSize:'20px'}}>Create Account</p>
+                <p style={{color:'white',fontWeight:700,fontSize:'20px'}}>{text}</p>
             </button>
         </div>
        
