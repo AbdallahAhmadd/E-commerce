c8d38c22882f61b4c58e88f2d65a46922a598713
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-20 02:09:44 +0300

    feat(ui): allow admin login with hardcoded credentials

--- frontend/src/Components/LoginForm/LoginForm.jsx
+++ frontend/src/Components/LoginForm/LoginForm.jsx
@@ -2,16 +2,36 @@ import './loginform.css'
 import Textbox from '../TextBox/Textbox';
 import Button from '../Button/button';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 function LoginForm(){
+
+    const[email,setEmail]=useState('')
+    const[password,setPassword]=useState('')
+    const history = useNavigate();
+
+    const handleLogin = (e)=>{
+        e.preventDefault();
+        const Adminemail = "admin"
+        const Adminpassword = "admin"
+
+        if(email === Adminemail && password === Adminpassword){
+            history('/ViewUsers')
+        }
+        else{ 
+            alert('Invalid Credentials')
+        }
+    }
+
     return(
         <>
         <div className='login-header'>
             <p>Login</p>
         </div>
         <div className='login-text-box-container'>
-            <Textbox placeholder='Email' type='email' />
-            <Textbox placeholder='Password' type='password' />
-            <Button text='Login' />
+            <Textbox placeholder='Email' type='email' onChange={(e) => setEmail(e.target.value)} value={email} />
+            <Textbox placeholder='Password' type='password' onChange={(e) => setPassword(e.target.value)} value={password} />
+            <Button text='Login' onClick={handleLogin}/>
             
         </div>
         <div className='sign-up-switch-container'>
