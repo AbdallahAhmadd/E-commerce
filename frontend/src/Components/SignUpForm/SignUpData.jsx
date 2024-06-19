b986add04668b97be7b258c4bfcd97a4e11363c9
Abdallah Ahmed <abdallah.ahmed@student.guc.edu.eg>
2024-06-19 15:02:54 +0300

    fix(server): add proxy server

--- frontend/src/Components/SignUpForm/SignUpData.jsx
+++ frontend/src/Components/SignUpForm/SignUpData.jsx
@@ -31,7 +31,7 @@ const SignUpData = () => {
             password: password
         };
 
-        const response = await fetch('http://localhost:3000/api/users', {
+        const response = await fetch('/api/users', {
             method:'POST',
             body: JSON.stringify(data),
             headers:{
